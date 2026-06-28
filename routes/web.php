94fac08c41446fe219e5c0404c1f0ea5b9f54c6d
Adi Pras <adiprsa@gmail.com>
2026-06-28 21:04:12 +0700

    Add feature login logout register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', function() {
+    if(Auth::check()) {
+        return redirect('/dashboard');
+    } else {
+        return redirect('/login');
+    }
+});
+
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
+
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
+
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
